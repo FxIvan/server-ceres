fb3289168158176c2ec2e3d560a81327540af16f
Ivan Ariel Almendra <almendraivan210814@gmail.com>
2022-12-03 14:55:42 -0300

    cambie puerto 3001 por 300 por que osino trae problema

--- .history/log/code/create_log_20221203140738.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-let today = new Date();
-let now = today.toLocaleString();
-
-var log_file = fs.createWriteStream(process.cwd() + `/log/syncrodb.log`, {flags : 'a'});
-log_file.write(`${now} | MSG | \n`)
-log_fileError.write(`${now} | ERR |  ${err} \n`)
--- .history/log/code/create_log_20221203140812.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-let today = new Date();
-let now = today.toLocaleString();
-
-var log_file = fs.createWriteStream(process.cwd() + `/log/syncrodb.log`, {flags : 'a'});
-var log_fileError = fs.createWriteStream(process.cwd() + `/log/syncrodbError.log`, {flags : 'a'});
-log_file.write(`${now} | MSG | \n`)
-log_fileError.write(`${now} | ERR |  ${err} \n`)
